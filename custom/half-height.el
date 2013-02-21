e2d0c970b0c7f4a10d5530a2712390e5fad8c575
Gavin M. Bell <gavin@llnl.gov>
2013-02-20 18:22:03 -0800

    Added quick elisp to change the window height of second frame.

---
+++ custom/half-height.el
@@ -0,0 +1,6 @@
+(defun halve-other-window-height ()
+  "Expand current window to use half of the other window's lines."
+  (interactive)
+  (enlarge-window (/ (window-height (next-window)) 2)))
+
+(global-set-key (kbd "C-c v") 'halve-other-window-height)
