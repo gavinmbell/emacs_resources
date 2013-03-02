ceff5503dbbc83c6f16bc13bf0f6747402373f1e
Gavin M. Bell <gavin@esgf.org>
2013-03-01 23:52:04 -0800

    added jtags and jtags-extras from maramalde package

---
+++ elpa/jtags-extras-0.3.0/jtags-extras-autoloads.el
@@ -0,0 +1,29 @@
+;;; jtags-extras-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (jtags-extras) "jtags-extras" "jtags-extras.el"
+;;;;;;  (20785 44701))
+;;; Generated autoloads from jtags-extras.el
+ (add-hook 'java-mode-hook 'jtags-extras)
+
+(autoload 'jtags-extras "jtags-extras" "\
+Load `jtags-extras'.
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("jtags-extras-pkg.el") (20785 44701 528742))
+
+;;;***
+
+(provide 'jtags-extras-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; jtags-extras-autoloads.el ends here
