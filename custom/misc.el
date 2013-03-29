45a8cdb32be987324ccc966c0dd18801d31bc18f
Gavin M. Bell <gavin@llnl.gov>
2013-03-29 16:35:11 -0700

    Added misc.el to repo as a place to park misc functions

---
+++ custom/misc.el
@@ -0,0 +1,26 @@
+;;-----------------------------------
+;; Launch a quick search to Google...
+;;-----------------------------------
+(defun google ()
+  "Google the selected region if any, display a query prompt otherwise."
+  (interactive)
+  (browse-url
+   (concat
+    "http://www.google.com/search?ie=utf-8&oe=utf-8&q="
+    (url-hexify-string (if mark-active
+         (buffer-substring (region-beginning) (region-end))
+       (read-string "Google: "))))))
+
+(global-set-key "\C-c\C-g" 'google)
+
+(defun visit-term-buffer ()
+  "Create or visit a terminal buffer."
+  (interactive)
+  (if (not (get-buffer "*ansi-term*"))
+      (progn
+        (split-window-sensibly (selected-window))
+        (other-window 1)
+        (ansi-term (getenv "SHELL")))
+    (switch-to-buffer-other-window "*ansi-term*")))
+
+(global-set-key "\C-ct" 'visit-term-buffer)
