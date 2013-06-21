5f0662f8317aa928899e49ac1b561514e18c82cd
Gavin M. Bell <gavin@esgf.org>
2013-06-21 03:39:57 -0700

    Added function for smart movement to beginning of line

--- custom/misc.el
+++ custom/misc.el
@@ -107,7 +107,7 @@
                                 (other-window -1)))
 
 ;;-----------------------------------
-;; Misc...
+;; flyspell...
 ;;-----------------------------------
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
@@ -124,3 +124,33 @@ buffer is not visiting a file."
     (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
 
 (global-set-key (kbd "C-x C-r") 'sudo-edit)
+
+;;-----------------------------------
+;; Smarter move to beginning of line
+;;-----------------------------------
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+  Move point to the first non-whitespace character on this line.
+  If point is already there, move to the beginning of the line.
+  Effectively toggle between the first non-whitespace character and
+  the beginning of the line.
+
+  If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+  point reaches the beginning or end of the buffer, stop there."
+
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+;; remap C-a to `smarter-move-beginning-of-line'
+(global-set-key [remap move-beginning-of-line] 'smarter-move-beginning-of-line)
