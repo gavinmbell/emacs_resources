f472726f32c49dc6918b9ec0d4d7d4ae9f759499
Gavin M. Bell <gavin@6thcolumn.org>
2013-11-15 02:14:26 -0800

    added a bunch of packages for themes and support utility functions for async and such

---
+++ elpa/edbi-0.1.1/edbi-autoloads.el
@@ -0,0 +1,18 @@
+;;; edbi-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("edbi-pkg.el" "edbi.el") (21057 18215
+;;;;;;  436804))
+
+;;;***
+
+(provide 'edbi-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; edbi-autoloads.el ends here
---
+++ elpa/epc-0.1.1/epc-autoloads.el
@@ -0,0 +1,17 @@
+;;; epc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("epc-pkg.el" "epc.el") (21057 18192 521561))
+
+;;;***
+
+(provide 'epc-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; epc-autoloads.el ends here
