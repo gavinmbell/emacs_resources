e0feba4ade23cd2bf8624f012c1d7b9bc532eb3a
Gavin M. Bell <gavin@soundcloud.com>
2014-12-03 15:08:56 +0100

    added slamhound to emacs

--- elpa/slamhound-2.0.0/slamhound-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; slamhound-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (slamhound) "slamhound" "slamhound.el" (20940 46794))
-;;; Generated autoloads from slamhound.el
-
-(autoload 'slamhound "slamhound" "\
-Run slamhound on the current buffer.
-
-  Requires active nrepl or slime connection.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("slamhound-pkg.el") (20940 46794 756762))
-
-;;;***
-
-(provide 'slamhound-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; slamhound-autoloads.el ends here
--- elpa/slamhound-2.0.0/slamhound-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "slamhound" "2.0.0" "Rip Clojure namespaces apart and rebuild them." (quote nil))
