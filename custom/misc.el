5740d15b2d30deaafffb9bfe5232bfea6c1b6a71
Gavin M. Bell <gavin@soundcloud.com>
2014-12-03 11:45:34 +0100

    Added kibit into misc.el

--- custom/misc.el
+++ custom/misc.el
@@ -195,3 +195,26 @@ Position the cursor at it's beginning, according to the current mode."
 
 (global-set-key [(control shift return)] 'smart-open-line-above)
 (global-set-key (kbd "M-O") 'smart-open-line-above)
+
+;;-----------------------------------
+;; Clojure tools for use through emacs
+;;-----------------------------------
+;; Teach compile the syntax of the kibit output
+(require 'compile)
+(add-to-list 'compilation-error-regexp-alist-alist
+         '(kibit "At \\([^:]+\\):\\([[:digit:]]+\\):" 1 2 nil 0))
+(add-to-list 'compilation-error-regexp-alist 'kibit)
+
+;; A convenient command to run "lein kibit" in the project to which
+;; the current emacs buffer belongs to.
+(defun kibit ()
+  "Run kibit on the current project.
+Display the results in a hyperlinked *compilation* buffer."
+  (interactive)
+  (compile "lein kibit"))
+
+(defun kibit-current-file ()
+  "Run kibit on the current file.
+Display the results in a hyperlinked *compilation* buffer."
+  (interactive)
+  (compile (concat "lein kibit " buffer-file-name)))
